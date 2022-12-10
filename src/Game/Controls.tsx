0f9b078166629d078a434abf016fd1e526ad2025
Samuel Reed <samuelreed01@gmail.com>
2022-12-10 20:06:56 +0700

    add moveable controls

--- src/Game/Controls.tsx
+++ src/Game/Controls.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import debounce from '../util/debounce';
 import { defaultGridSize, maxGridSize, minGridSize } from './defaults';
 
@@ -21,6 +21,10 @@ export default function Controls({
 }: IControlsProps) {
   const [width, setWidth] = useState(defaultGridSize);
   const [height, setHeight] = useState(defaultGridSize);
+  const mouseStartX = useRef<number>(0);
+  const mouseStartY = useRef<number>(0);
+  const stickToMouse = useRef<boolean>(false);
+  const controlsRef = useRef<HTMLDivElement>(null);
 
   function handleSetWidth(event: React.ChangeEvent<HTMLInputElement>) {
     const newWidth = Number(event.target.value);
@@ -43,17 +47,40 @@ export default function Controls({
     }
     debounce(adjustAndSet);
   }
+
+  function handleStartMove(event: React.MouseEvent) {
+    mouseStartX.current = event.clientX;
+    mouseStartY.current = event.clientY;
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', (event: MouseEvent) => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    });
+  }
+
+  function handleMouseMove(event: MouseEvent) {
+    if (controlsRef.current) {
+      const movedXBy = event.clientX - mouseStartX.current;
+      const movedYBy = event.clientY - mouseStartY.current;
+      mouseStartX.current = event.clientX;
+      mouseStartY.current = event.clientY;
+      controlsRef.current.style.left = `${controlsRef.current.offsetLeft + movedXBy}px`;
+      controlsRef.current.style.top = `${controlsRef.current.offsetTop + movedYBy}px`;
+    }
+  }
+
   return (
     <div className="controls">
-      <div className="controls-buttons">
-        <button onClick={stepGame}>Step</button>
-        <button onClick={startGame}>Start</button>
-        <button onClick={stopGame}>Stop</button>
-        <button onClick={clearCells}>Clear</button>
-      </div>
-      <div className="controls-inputs">
-        <input type="number" value={width} onChange={handleSetWidth} />
-        <input type="number" value={height} onChange={handleSetHeight} />
+      <div ref={controlsRef} className="moveable-controls" onMouseDown={handleStartMove}>
+        <div className="controls-buttons">
+          <button onClick={stepGame}>Step</button>
+          <button onClick={startGame}>Start</button>
+          <button onClick={stopGame}>Stop</button>
+          <button onClick={clearCells}>Clear</button>
+        </div>
+        <div className="controls-inputs">
+          <input type="number" value={width} onChange={handleSetWidth} />
+          <input type="number" value={height} onChange={handleSetHeight} />
+        </div>
       </div>
     </div>
   );
